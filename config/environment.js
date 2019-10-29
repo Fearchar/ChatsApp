d101b0f8fa17423e5fcc841d83740456992caa0c
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-10-29 14:34:26 +0000

    Removed environment from cach and adding git ignore.

--- config/environment.js
+++
@@ -1,6 +0,0 @@
-const port = process.env.PORT || 4000
-const env = process.env.NODE_ENV || 'development'
-const dbURI = process.env.MONGODB_URI || `mongodb://localhost:27017/ChatsApp-db-${env}`
-const secret = process.env.SECRET || '^HEARki?nd££l123$23i2jnimenbp'
-
-module.exports = { port, dbURI, secret }
