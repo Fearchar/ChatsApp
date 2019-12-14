4f9410cd6afa6adf78cbf520d10575df1f2e17cc
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-12-14 12:54:33 +0000

    ThreadsPane search filter built.

--- src/components/user panel/ThreadsPanel.js
+++ src/components/user panel/ThreadsPanel.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import moment from 'moment'
 
 import ThreadBox from './ThreadBox'
@@ -6,30 +6,49 @@ import { Form, Field } from '../common/Form'
 import lastItem from '../../lib/lastItem'
 
 const ThreadsPanel = ({ threads, focusThread, setFocusThread }) => {
+  const [ fields, setFields ] = useState({})
+
   function lastMessageSort(array) {
 
     return array.sort((threadA, threadB) => {
-      const msA = threadA.messages.length &&
+      const aMs = threadA.messages.length &&
         moment(lastItem(threadA.messages).createdAt).valueOf()
-      const msB = threadB.messages.length &&
+      const bMs = threadB.messages.length &&
         moment(lastItem(threadB.messages).createdAt).valueOf()
 
-      return msB - msA
+      return bMs - aMs
+    })
+  }
+
+  function filterThreads(threads, { search }) {
+    const re = new RegExp(search, 'i')
+
+    return !search ? threads : threads.filter(thread => {
+      return (
+        re.test(thread.name) ||
+        thread.participants.some(p => re.test(p.name)) ||
+        thread.admins.some(a => re.test(a.name))
+      )
     })
   }
 
   return (
     <div>
-      <Form fields={[ new Field('', 'text', 'Search') ]} />
+      <Form
+        hasBox={true}
+        buttonName="Search"
+        fields={[ new Field('', 'text', 'Search', 'search') ]}
+        fieldSetter={setFields}
+      />
       <div className="card scrolls">
-        {threads && lastMessageSort(threads).map(thread =>
-          <ThreadBox
-            key={thread._id}
-            thread={thread}
-            isFocus={thread === focusThread}
-            setFocusThread={setFocusThread}
-          />
-        )}
+        {threads && lastMessageSort(filterThreads(threads, fields))
+          .map(thread =>
+            <ThreadBox
+              key={thread._id}
+              thread={thread}
+              isFocus={thread === focusThread}
+              setFocusThread={setFocusThread}
+            />)}
       </div>
     </div>
   )
