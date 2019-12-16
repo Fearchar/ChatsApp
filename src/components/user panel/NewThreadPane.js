6699c833d11200a53d88eef8b098e8934cb9beab
Fearchar MacLean <fearchar.maclean@gmail.com>
2019-12-16 14:36:25 +0000

    Started on thread creation functions in NewThreadPane.

--- src/components/user panel/NewThreadPane.js
+++ src/components/user panel/NewThreadPane.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 
 import { Form, Field } from '../common/Form'
 import ContactBox from './ContactBox'
 
-const NewThreadPane = ({ contacts }) => {
+const NewThreadPane = ({ threads, contacts }) => {
   const [ fields, setFields ] = useState({})
+
+  //!!! should probably change name
+  function has121Thread(threads, contact) {
+    return threads.some(thread => {
+      const users = [ ...thread.participants, ...thread.admins ]
+      return users.length < 3 && users.some(user => user.name === contact.name)
+    })
+  }
+
+  function createThread(participants) {
+  }
+
+  //!!! should probably change name
+  //!!! the logic to stop double threads should probably take place on the backend
+  function contactThread(user, contact) {
+    if (!has121Thread(user.threads, contact)) createThread(contact)
+  }
 
   function filterContacts(contacts, { search }) {
     const regex = new RegExp(search, 'i')
@@ -22,14 +40,11 @@ const NewThreadPane = ({ contacts }) => {
       />
       <div className="card scrolls">
         {contacts && filterContacts(contacts, fields)
-          .map(contact => {
-            console.log(contact)
-            return (
-              <ContactBox
-                key={contact._id}
-                {...contact}
-              />)
-          })}
+          .map(contact =>
+            <ContactBox
+              key={contact._id}
+              {...contact}
+            />)}
       </div>
     </div>
   )
